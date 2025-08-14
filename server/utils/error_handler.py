1cff3612fb7d5e47c3f938e9481508f22dbb2695
Damian Dolata <72529225+damian00dolata@users.noreply.github.com>
2025-08-14 23:51:51 +0200

    Refactor Docker configuration, update README, and enhance logging functionality (#1)

--- server/utils/error_handler.py
+++ server/utils/error_handler.py
@@ -3,7 +3,7 @@ from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
 import traceback
-from logger import logger
+from utils.debug_logger import logger
 
 def add_exception_handlers(app: FastAPI, debug: bool = False):
 
